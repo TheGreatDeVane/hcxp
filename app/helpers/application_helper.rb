c93ab03925c556530691c03bc377289bd4db37ec
Mike Bajur <mike@vbot.tv>
2014-05-18 22:59:07 +0200

    Fix default gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,9 +10,8 @@ module ApplicationHelper
     # if user.avatar_url.present?
     #   user.avatar_url
     # else
-      default_url = "#{root_url}images/guest.png"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{options[:size]}&d=#{CGI.escape(default_url)}"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{options[:size]}&d=mm"
     # end
   end
 end
