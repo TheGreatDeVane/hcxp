41b66f2a472f71c99626b15a8b4e14980b0ef910
Mike Bajur <mike@vbot.tv>
2014-05-19 21:46:32 +0200

    Remove previous migration

--- db/migrate/20140519143222_add_medium_poster_size_to_events_uploader.rb
+++
@@ -1,9 +0,0 @@
-class AddMediumPosterSizeToEventsUploader < ActiveRecord::Migration
-  def change
-    Event.reset_column_information
-    Event.all.each do |e|
-      puts "Updating Event #{e.id}"
-      e.update_attribute :remote_poster_url, e.poster.url
-    end
-  end
-end
