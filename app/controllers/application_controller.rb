782cb4f79247aa9f4b6e84e6fb42addfe4d53c38
Mike Bajur <mike@vbot.tv>
2014-05-19 23:22:49 +0200

    Initial translation attempt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :get_cities
-
-  # Fetch cities (based on venues) to populate
-  # sidebar's 'Cities' list.
-  def get_cities
-    @cities = Venue.select('DISTINCT(city), country_code')
-  end
+  before_action :set_locale
 
   def style_guide; end
+
+  private
+
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
 end
